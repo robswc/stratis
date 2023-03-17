928642a9f86f9ca06e91edb4954d2c73b84e281d
robwsc <robert.s.w.carroll@gmail.com>
2023-03-17 11:27:45 -0400

    fix imports

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from api.api_v1.endpoints import data
-from app.api.api_v1.endpoints import strategy
+from api.api_v1.endpoints import strategy
 
 api_router = APIRouter()
 api_router.include_router(strategy.router, prefix="/strategy", tags=["strategy"])
